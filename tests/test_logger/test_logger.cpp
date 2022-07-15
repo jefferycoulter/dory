6ded1f32c16205ccc93dd373e491b00d1ba8edf8
JefferyCoulter <jrcoulter2@gmail.com>
2022-07-15 16:50:40 -0400

    updated tests

--- tests/test_logger/test_logger.cpp
+++ tests/test_logger/test_logger.cpp
@@ -1,16 +1,27 @@
 #include <dory.h>
 
-
-int main()
+class TestApp : public DORY::Application
 {
-    int var1 = 1;
-    char var2[] = "string";
-    DORY::DTRACE("This is a trace message.", var1, var2);
-    DORY::DDEBUG("This is a debug message.", var1, var2);
-    DORY::DINFO("This is a info message.", var1, var2);
-    DORY::DWARN("This is a warn message.", var1, var2);
-    DORY::DERROR("This is a error message.", var1, var2);
-    DORY::DFATAL("This is a fatal message.", var1, var2);
+public:
+    TestApp()
+    {
+        int var1 = 1;
+        char var2[] = "string";
+        DORY::DTRACE("This is a trace message.", var1, var2);
+        DORY::DDEBUG("This is a debug message.", var1, var2);
+        DORY::DINFO("This is a info message.", var1, var2);
+        DORY::DWARN("This is a warn message.", var1, var2);
+        DORY::DERROR("This is a error message.", var1, var2);
+        DORY::DFATAL("This is a fatal message.", var1, var2);
+    }
 
-    return 0;
+    ~TestApp()
+    {
+        DORY::DTRACE("Goodbye World!");
+    }
+};
+
+DORY::Application* DORY::Init()
+{
+    return new TestApp();
 }
